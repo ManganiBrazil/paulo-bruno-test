2f472fe19bbb70eb858794d76141fda98aae92d2
ManganiBrazil <singlepattern@gmail.com>
2024-02-12 03:00:53 -0300

    paulo bruno test

--- src/main/java/com/axreng/backend/Main.java
+++ src/main/java/com/axreng/backend/Main.java
@@ -10,19 +10,21 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class Main {
-    public static void main(String[] args) {
 
-        final KeywordService keywordService = new KeywordService();
-        final SearchService searchService = new SearchService();
+    public static void main(String[] args) {
 
         get("/crawl/:id", (req, res) -> {
             res.type("application/json");
+
+            SearchService searchService = new SearchService();
             SearchData searchData = searchService.search(req.params(":id"), System.getenv("url"));
             return new Gson().toJson(searchData);
         });
 
         post("/crawl", (req, res) -> {
             res.type("application/json");
+
+            KeywordService keywordService = new KeywordService();
             KeywordRequestModel keywordRequest = new Gson().fromJson(req.body(), KeywordRequestModel.class);
             return new Gson().toJson(keywordService.decode(keywordRequest));
         });
